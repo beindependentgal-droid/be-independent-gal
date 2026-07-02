bbb82a93954404fe7bc72a8e4a16b6296fe36583
Be Independent Gal <beindependentgal@gmail.com>
2026-07-02 19:38:12 +0300

    Your update

--- app/layout.tsx
+++ app/layout.tsx
@@ -129,6 +129,9 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="BIG" />
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/images/bigFavicon.png" sizes="512x512" type="image/png" />
+        <link rel="shortcut icon" href="/images/bigFavicon.png" />
+        <link rel="apple-touch-icon" href="/images/bigFavicon.png" sizes="512x512" />
       </head>
 
       <body className={`${outfit.variable} ${inter.variable} font-inter antialiased bg-background text-foreground`}>
